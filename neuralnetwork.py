3d3a527d50dde7755ac479b3a60476399f6ae29f
cytric-74 <rohansix677@gmail.com>
2025-06-09 19:45:03 +0530

    ini

---
+++ neuralnetwork.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+def sigmoid(x):
+    return 1/(1 + math.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return x * (x-1)
+
+def init_networking():
+    network = {
+        "input_hidden_weights": [[random.uniform(-1, 1) for _ in range(2)] for _ in range(2)],
+        "hidden_output_weights": [random.uniform(-1, 1) for _ in range(2)],
+        "hidden_bias": [random.uniform(-1, 1) for _ in range(2)],
+        "output_bias": random.uniform(-1, 1)
+    }
+    return network
+
+def forward_pass(inputs, net):
+    hidden_in = []
+    hidden_out = []
+
+    for i in range(2)
+        summation = sum(inputs[j] * net["input_hidden_weights"][j][i] for j in range(2)) + net["hidden_bias"][i]
+        hidden_in.append(summation)
+        hidden_out.append(sigmoid(summation))
